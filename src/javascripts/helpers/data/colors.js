2e08536acec3b358492cbfcb6d89c8b0b2c32fc6
Charity Bunyon <cseymou5@gmail.com>
2019-10-14 19:04:31 -0500

    added array

---
+++ src/javascripts/helpers/data/colors.js
@@ -0,0 +1,43 @@
+const colors = [
+  {
+    id: 'color1',
+    name: 'red',
+    hexValue: 'ff0000',
+    upVotes: 15,
+  },
+  {
+    id: 'color2',
+    name: 'orange',
+    hexValue: 'ff7f00',
+    upVotes: 3,
+  },
+  {
+    id: 'color3',
+    name: 'yellow',
+    hexValue: 'ffff00',
+    upVotes: 2,
+  },
+  {
+    id: 'color4',
+    name: 'green',
+    hexValue: '00ff00',
+    upVotes: 6,
+  },
+  {
+    id: 'color5',
+    name: 'blue',
+    hexValue: '0000ff',
+    upVotes: 25,
+  },
+  {
+    id: 'color6',
+    name: 'violet',
+    hexValue: '9400d3',
+    upVotes: 11,
+  },
+];
+
+
+const getColors = () => colors;
+
+export default { getColors };
